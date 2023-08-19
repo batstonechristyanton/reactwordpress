41b1aa5e0e8e4d7d71bda1100a1b6a8783f08aed
batstonechristyanton <bchristyanton@trentu.ca>
2023-08-19 15:18:25 -0400

    finished some stuff

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,33 +2,34 @@ import React, { Component } from 'react'
 import axios from 'axios';
 
 export class Posts extends Component {
-   state = {
-       posts: [],
-       isLoaded: false
-   }
+    state = {
+        posts: [],
+        isLoaded: false
+    } 
+    componentDidMount() { 
+        axios.get('http://oddvoxel.ca/styleguide-test/wp-json/wp/v2/posts')
+            .then(res => this.setState({
+                posts: res.data,
+                isLoaded: true
 
- componentDidMount () {
- axios.get('http://192.168.1.113:8080/wp-json/wp/v2/posts')
-       .then(res => this.setState({
-           posts: res.data,
-           isLoaded: true
-       }))
-       .catch(err => console.log(err))
-   }
+            }))
+            .catch(err => console.log(err))
+    }
 
 
-   render() {
-       const {posts, isLoaded} = this.state;
-       return (
-           <div>
-               {posts.map(post =>
-               <h4>{post.title.rendered}</h4>
-               )} 
-               {posts.map(post =>
-               <h4>{post.content.rendered}</h4>
-               )}
-           </div>
-       );
-   }
+    render() {
+        const { posts, isLoaded } = this.state;
+        return ( 
+            
+            <div>
+                {posts.map(post =>
+                    <h4>{post.title.rendered}</h4>
+                )}
+                {posts.map(post =>
+                    <h4>{post.content.rendered}</h4>
+                )}
+            </div>
+        );
+    }
 }
 export default Posts
