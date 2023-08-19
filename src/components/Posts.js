296200e055865a177824f2ed77b5cbf1fccedb8d
batstonechristyanton <bchristyanton@trentu.ca>
2023-08-19 12:38:50 -0400

    finished react app

---
+++ src/components/Posts.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export class Posts extends Component {
+   state = {
+       posts: [],
+       isLoaded: false
+   }
+
+ componentDidMount () {
+ axios.get('http://192.168.1.113:8080/wp-json/wp/v2/posts')
+       .then(res => this.setState({
+           posts: res.data,
+           isLoaded: true
+       }))
+       .catch(err => console.log(err))
+   }
+
+
+   render() {
+       const {posts, isLoaded} = this.state;
+       return (
+           <div>
+               {posts.map(post =>
+               <h4>{post.title.rendered}</h4>
+               )} 
+               {posts.map(post =>
+               <h4>{post.content.rendered}</h4>
+               )}
+           </div>
+       );
+   }
+}
+export default Posts
